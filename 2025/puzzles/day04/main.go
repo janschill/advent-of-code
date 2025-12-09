29b9572c9d98a6f99e676bbe7e34e27c8bf7cbf5
Jan <janschill@proton.me>
2025-12-09 17:11:52 +0100

    Day 4

--- 2025/puzzles/day04/main.go
+++ 2025/puzzles/day04/main.go
@@ -16,18 +16,99 @@ func main() {
 	fmt.Println("Part 2: ", part2(lines))
 }
 
-func part1(lines []string) int {
-	for _, line := range lines {
-		log.Default().Println(line)
+func toGrid(lines []string) [][]rune {
+	grid := make([][]rune, len(lines))
+	for i, line := range lines {
+		grid[i] = []rune(line)
 	}
-	return 0
+	return grid
+}
+
+func part1(lines []string) int {
+	grid := toGrid(lines)
+	rows, cols := len(grid), len(grid[0])
+	sum := 0
+
+	for r := 0; r < rows; r++ {
+		for c := 0; c < cols; c++ {
+			rollCount := 0
+			cur := grid[r][c]
+			if cur == '@' {
+				for dr := -1; dr <= 1; dr++ {
+					for dc := -1; dc <= 1; dc++ {
+						if dr == 0 && dc == 0 {
+							continue
+						}
+						nr, nc := r+dr, c+dc
+						if nr < 0 || nr >= rows || nc < 0 || nc >= cols {
+							continue
+						}
+						neighbor := grid[nr][nc]
+						if neighbor == '@' {
+							rollCount += 1
+						}
+						_ = neighbor
+					}
+				}
+				if rollCount < 4 {
+					sum += 1
+					// fmt.Printf("(%d,%d)", r, c)
+					// fmt.Println()
+				}
+			}
+		}
+	}
+	return sum
 }
 
 func part2(lines []string) int {
-	for _, line := range lines {
-		log.Default().Println(line)
+	grid := toGrid(lines)
+	rows, cols := len(grid), len(grid[0])
+	sum := 0
+	var changed bool
+
+	for {
+		var changedCells [][]int
+		for r := 0; r < rows; r++ {
+			for c := 0; c < cols; c++ {
+				rollCount := 0
+				cur := grid[r][c]
+				if cur == '@' {
+					for dr := -1; dr <= 1; dr++ {
+						for dc := -1; dc <= 1; dc++ {
+							if dr == 0 && dc == 0 {
+								continue
+							}
+							nr, nc := r+dr, c+dc
+							if nr < 0 || nr >= rows || nc < 0 || nc >= cols {
+								continue
+							}
+							neighbor := grid[nr][nc]
+							if neighbor == '@' {
+								rollCount += 1
+							}
+							_ = neighbor
+						}
+					}
+					if rollCount < 4 {
+						sum += 1
+						changedCells = append(changedCells, []int{r, c})
+						changed = true
+					}
+				}
+			}
+		}
+		if !changed {
+			break
+		}
+		for i := 0; i < len(changedCells); i++ {
+			x, y := changedCells[i][0], changedCells[i][1]
+			grid[x][y] = '.'
+		}
+		changed = false
 	}
-	return 0
+
+	return sum
 }
 
 func puzzleDir() string {
